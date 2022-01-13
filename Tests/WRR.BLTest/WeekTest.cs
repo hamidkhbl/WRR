bd9629a984bb59d691c2624b043d3af3ceb76966
Hamid <hamidkhbl@gmail.com>
2022-01-13 11:04:10 -0800

    Add Week Class

---
+++ Tests/WRR.BLTest/WeekTest.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using WRR.BL;
+
+namespace WRR.BLTest
+{
+    [TestClass]
+    public class WeekTest
+    {
+        [TestMethod]
+        public void IsValidValidWeek()
+        {
+            // Arrange
+            Week week = new Week
+            {
+                StartDate = new DateTime(year:2022, month:1, day:1),
+                EndDate = new DateTime(year: 2022, month: 1, day: 7)
+            };
+            // Act
+            bool expected = true;
+            bool actual = week.Validate();
+            // Asset
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void IsValidInValidWeek()
+        {
+            // Arrange
+            Week week = new Week
+            {
+                StartDate = new DateTime(year: 2022, month: 1, day: 1),
+                EndDate = new DateTime(year: 2022, month: 1, day: 6)
+            };
+            // Act
+            bool expected = false;
+            bool actual = week.Validate();
+            // Asset
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GenerateDatesValidWeek()
+        {
+            // Arrange
+            Week week = new Week
+            {
+                StartDate = new DateTime(year: 2022, month: 1, day: 1),
+                EndDate = new DateTime(year: 2022, month: 1, day: 7)
+            };
+            List<DateTime> expected = new List<DateTime>();
+            expected.Add(new DateTime(year: 2022, month: 1, day: 1));
+            expected.Add(new DateTime(year: 2022, month: 1, day: 2));
+            expected.Add(new DateTime(year: 2022, month: 1, day: 3));
+            expected.Add(new DateTime(year: 2022, month: 1, day: 4));
+            expected.Add(new DateTime(year: 2022, month: 1, day: 5));
+            expected.Add(new DateTime(year: 2022, month: 1, day: 6));
+            expected.Add(new DateTime(year: 2022, month: 1, day: 7));
+
+            // Act
+            week.GenerateDates();
+            List<DateTime> actual = week.days;
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void GenerateDatesInValidWeek()
+        {
+            // Arrange
+            Week week = new Week
+            {
+                StartDate = new DateTime(year: 2022, month: 1, day: 1),
+                EndDate = new DateTime(year: 2022, month: 1, day: 6)
+            };
+            List<DateTime> expected = null;
+
+            // Act
+            week.GenerateDates();
+            List<DateTime> actual = week.days;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ WRR/WRR.BL/Week.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WRR.BL
+{
+    public class Week
+    {
+        public Week()
+        {
+
+        }
+
+        public Week(int weekId)
+        {
+            WeekId = weekId;
+        }
+
+        public int WeekId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<DateTime> days { get; private set; }
+
+        public bool Validate()
+        {
+            bool isValid = false;
+            var timeDiff = (EndDate - StartDate).Days;
+            Console.WriteLine(timeDiff.ToString());
+            if (timeDiff == 6)
+                isValid = true;
+            return isValid;
+
+        }
+        public void GenerateDates()
+        {
+            if (Validate())
+            {
+                days = new List<DateTime>();
+                for (int i = 0; i < 7; i++)
+                {
+                    days.Add(StartDate.AddDays(i));
+                }
+            }
+            else
+            {
+                days = null;
+                // TODO: Log error
+            }
+        }
+    }
+}
