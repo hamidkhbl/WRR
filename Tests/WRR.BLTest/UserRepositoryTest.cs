1028557bd1f0e5f32bb280b7cc18be479070fb6a
Hamid <hamidkhbl@gmail.com>
2022-01-14 11:53:57 -0800

    Add UserRepository class

---
+++ Tests/WRR.BLTest/UserRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WRR.BL;
+
+namespace WRR.BLTest
+{
+    [TestClass]
+    public class UserRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            // Arrange
+            var userRepository = new UserRepository();
+            var expected = new User(1)
+            {
+                Email = "hamidkhbl@gmail.com",
+                FirstName = "Hamid",
+                LastName = "Khbl"
+            };
+
+            // Act
+            var actual = userRepository.Retrieve(1);
+
+            // Assert
+            Assert.AreEqual(expected.UserId, actual.UserId);
+            Assert.AreEqual(expected.Email, actual.Email);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+        }
+    }
+}
---
+++ WRR/WRR.BL/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WRR.BL
+{
+    public class UserRepository
+    {
+        public User Retrieve(int userId)
+        {
+            User user = new User(userId);
+
+            if(userId == 1)
+            {
+                user.Email = "hamidkhbl@gmail.com";
+                user.FirstName = "Hamid";
+                user.LastName = "Khbl";
+            }
+
+            return user;
+        }
+
+        public bool Save(User user)
+        {
+            return true;
+        }
+    }
+}
